d09c876a433bbfb1510f452313af1095d92c64c4
MalPony <ilya30ilya30@gmail.com>
2026-07-12 17:35:34 +0400

    Update flask_webhook.py

--- flask_webhook.py
+++ flask_webhook.py
@@ -171,7 +171,7 @@ if __name__ == '__main__':
         def load(self):
             return self.application
 
-    port = int(os.getenv("PORT", 8080))
+    port = int(os.getenv("PORT", 5000))
     options = {
         'bind': f'0.0.0.0:{port}',
         'workers': 1,
