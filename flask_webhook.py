e3d4c68573edb954f13a09d8873be2144a9c0923
MalPony <ilya30ilya30@gmail.com>
2026-07-12 17:07:47 +0400

    Update flask_webhook.py

--- flask_webhook.py
+++ flask_webhook.py
@@ -124,4 +124,4 @@ async def setup_webhook():
 
 if __name__ == '__main__':
     port = int(os.getenv("PORT", 8080))
-    web.run_app(app, host="0.0.0.0", port=port)
+    app.run(host='0.0.0.0', port=port, debug=False)
